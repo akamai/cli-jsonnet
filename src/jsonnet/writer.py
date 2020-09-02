ffe92de6a4e07d65010ac755a6c8039e034b1ba0
Anthony Hogg <ahogg@akamai.com>
2020-09-02 17:30:32 +0200

    feat: format jsonnet output (if jsonnetfmt is installed)

--- src/jsonnet/writer.py
+++ src/jsonnet/writer.py
@@ -6,9 +6,20 @@ if py3:
 else:
   from StringIO import StringIO
 
+from subprocess import Popen, PIPE
+
 class JsonnetWriter(StringIO):
   def __init__(self):
     super(JsonnetWriter, self).__init__()
 
   def writeln(self, s = ""):
     self.write(s + "\n")
+
+  def getvalue(self):
+    val = super(JsonnetWriter, self).getvalue()
+    try:
+      val = Popen(["jsonnetfmt", "-"], stdout=PIPE, stdin=PIPE).communicate(input=val.encode())[0]
+      val = val.decode("utf-8")
+    except:
+      pass
+    return val
