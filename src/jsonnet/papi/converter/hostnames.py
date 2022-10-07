1c219323e9b8d021c58488ca59c4e9281912e288
guillaume-fr <guillaume-fr@users.noreply.github.com>
2022-10-07 10:15:47 +0200

    fix ipBehavior case to camel (was snake case)

--- src/jsonnet/papi/converter/hostnames.py
+++ src/jsonnet/papi/converter/hostnames.py
@@ -19,8 +19,8 @@ class HostnamesConverter(BaseConverter):
           ip_behavior = edgeHostnames.get(mapping.get('cnameTo')).get('ipVersionBehavior')
           writer.writeln('ipBehavior: {},'.format(json.dumps(ip_behavior)))
         else:
-          writer.writeln('// failed to determine ip_behavior, please fill in manually')
-          writer.writeln("ip_behavior: error 'ip_behavior is required',")
+          writer.writeln('// failed to determine ipBehavior, please fill in manually')
+          writer.writeln("ipBehavior: error 'ipBehavior is required',")
         if cnameTo.endswith('edgekey.net'):
           writer.writeln("// edgekey.net Edge Hosnames require the certificate enrollment id")
           writer.writeln("// to be provided, which can be retrieved using the Akamai CPS CLI")
